a58eb909e4f2e1f1f30a55f290e3c8bc6318b11d
Ziya Mollamahmut <info@ziyad.info>
2019-12-05 16:59:02 +0300

    text outline

--- LazZiya.ImageResize/Watermark/TextWM.cs
+++ LazZiya.ImageResize/Watermark/TextWM.cs
@@ -31,7 +31,7 @@ namespace LazZiya.ImageResize.Watermark
         /// See <see cref="TargetSpot"/></param>
         /// <param name="style">Font style</param>
         /// <param name="margin">The distance in pixels between the watermark text and the nearest border of the main image.</param>
-        public static void TextWatermark(this Image img,
+        /*public static void TextWatermark(this Image img,
             string text,
             string color = "#77FFFFFF", string bgColor = "#00000000",
             string fontFamily = "Arial", int size = 24,
@@ -65,6 +65,83 @@ namespace LazZiya.ImageResize.Watermark
             graphics.DrawString(text, textFont, textBrush, drawPoint);
 
             graphics.Dispose();
+        }
+        */
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="img"></param>
+        /// <param name="text"></param>
+        /// <param name="color"></param>
+        /// <param name="bgColor"></param>
+        /// <param name="outlineColor"></param>
+        /// <param name="fontFamily"></param>
+        /// <param name="size"></param>
+        /// <param name="spot"></param>
+        /// <param name="outlineWidth"></param>
+        /// <param name="style"></param>
+        /// <param name="margin"></param>
+        public static void TextWatermark(this Image img,
+            string text,
+            string color = "#77FFFFFF", string bgColor = "#00000000", string outlineColor = "#00000000",
+            string fontFamily = "Arial", int size = 24, float outlineWidth = 1.0f,
+            TargetSpot spot = TargetSpot.BottomLeft, FontStyle style = FontStyle.Regular, int margin = 10)
+        {
+
+
+            var _bgAlpha = int.Parse(bgColor.Substring(1, 2), NumberStyles.HexNumber);
+            var _bgColor = bgColor.Substring(3, 6);
+            var bgBrush = new SolidBrush(Color.FromArgb(_bgAlpha, ColorTranslator.FromHtml($"#{_bgColor}")));
+            var bgPos = SetBGPos(img.Width, img.Height, size, spot, margin);
+            
+
+            var textFont = new Font(fontFamily, size, style, GraphicsUnit.Pixel);
+            var _alpha = int.Parse(color.Substring(1, 2), NumberStyles.HexNumber);
+            var _color = color.Substring(3, 6);
+            var textBrush = new SolidBrush(Color.FromArgb(_alpha, ColorTranslator.FromHtml($"#{_color}")));
+
+            var _outlineAlpha = int.Parse(outlineColor.Substring(1, 2), NumberStyles.HexNumber);
+            var _outlineColor = outlineColor.Substring(3, 6);
+            var outlineBrush = new SolidBrush(Color.FromArgb(_outlineAlpha, ColorTranslator.FromHtml($"#{_outlineColor}")));
+
+            var sf = new StringFormat()
+            {
+                FormatFlags = StringFormatFlags.NoWrap
+            };
+
+            var fFamily = new FontFamily(fontFamily);
+
+            using (var graphics = Graphics.FromImage(img))
+            {
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                //graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+                //graphics.TextContrast = 12;
+                //graphics.CompositingMode = CompositingMode.SourceOver;
+                //graphics.InterpolationMode = InterpolationMode.High;
+
+                // Draw background
+                graphics.FillRectangle(bgBrush, bgPos);
+
+                // Measure text size
+                var textMetrics = graphics.MeasureString(text, textFont, img.Width, sf);
+                var beforeText = SetTextAlign(textMetrics, img.Width, spot);
+                var drawPoint = new PointF(beforeText, bgPos.Y + (bgPos.Height / 4));
+
+                using (var pen = new Pen(outlineBrush, outlineWidth))
+                {
+                    using (var p = new GraphicsPath())
+                    {
+                        p.AddString(text, fFamily, (int)style, size, drawPoint, sf);
+                        
+                        // Draw text outline
+                        graphics.DrawPath(pen, p);
+
+                        // Draw text
+                        graphics.FillPath(textBrush, p);
+                    }
+                }
+            }
         }
 
 
@@ -108,7 +185,7 @@ namespace LazZiya.ImageResize.Watermark
 
             return rect;
         }
-        
+
         private static int SetTextAlign(SizeF textMetrics, int imgWidth, TargetSpot spot)
         {
             int space;
