ac97e518894f2023e51345ea86997b2d88a17762
Ziya Mollamahmut <ziyadmm@gmail.com>
2019-12-08 20:37:09 +0300

    GraphicOptions for resizing methods

--- LazZiya.ImageResize/ImageResize.cs
+++ LazZiya.ImageResize/ImageResize.cs
@@ -1,6 +1,5 @@
 ﻿using LazZiya.ImageResize.Exceptions;
 using LazZiya.ImageResize.ResizeMethods;
-using LazZiya.ImageResize.Tools;
 using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
@@ -22,9 +21,41 @@ namespace LazZiya.ImageResize
         /// <param name="newWidth"></param>
         /// <param name="newHeight"></param>
         /// <returns></returns>
-        public static Image Scale(Image img, int newWidth, int newHeight)
+        public static Image Scale(this Image img, int newWidth, int newHeight)
         {
             var resize = new Scale(img.Size, new Size(newWidth, newHeight));
+
+            return Resize(img, resize.SourceRect, resize.TargetRect);
+        }
+
+        /// <summary>
+        /// Auto scale image by width or height till longest border (width/height) is equal to new width/height.
+        /// Final image aspect ratio is equal to original image aspect ratio.
+        /// If the aspect ratio of new w/h != aspect ratio of original image then 
+        /// one border will be in different size than the given value in order to keep original aspect ratio
+        /// </summary>
+        /// <param name="img"></param>
+        /// <param name="newWidth"></param>
+        /// <param name="newHeight"></param>
+        /// <param name="ops">Graphic options <see cref="GraphicOptions"/></param>
+        /// <returns></returns>
+        public static Image Scale(this Image img, int newWidth, int newHeight, GraphicOptions ops)
+        {
+            var resize = new Scale(img.Size, new Size(newWidth, newHeight));
+
+            return Resize(img, resize.SourceRect, resize.TargetRect, ops);
+        }
+
+        /// <summary>
+        /// Scale image by width and keep same aspect ratio of target image same as the original image.
+        /// Height will be adjusted automatically
+        /// </summary>
+        /// <param name="img"></param>
+        /// <param name="newWidth"></param>
+        /// <returns></returns>
+        public static Image ScaleByWidth(this Image img, int newWidth)
+        {
+            var resize = new Scale(img.Size, new Size(newWidth, 0));
 
             return Resize(img, resize.SourceRect, resize.TargetRect);
         }
@@ -35,10 +66,25 @@ namespace LazZiya.ImageResize
         /// </summary>
         /// <param name="img"></param>
         /// <param name="newWidth"></param>
+        /// <param name="ops">Graphic options <see cref="GraphicOptions"/></param>
         /// <returns></returns>
-        public static Image ScaleByWidth(Image img, int newWidth)
+        public static Image ScaleByWidth(this Image img, int newWidth, GraphicOptions ops)
         {
             var resize = new Scale(img.Size, new Size(newWidth, 0));
+
+            return Resize(img, resize.SourceRect, resize.TargetRect, ops);
+        }
+
+        /// <summary>
+        /// Scale image by height and keep same aspect ratio of target image same as the original image.
+        /// Width will be adjusted automatically
+        /// </summary>
+        /// <param name="img"></param>
+        /// <param name="newHeight"></param>
+        /// <returns></returns>
+        public static Image ScaleByHeight(this Image img, int newHeight)
+        {
+            var resize = new Scale(img.Size, new Size(0, newHeight));
 
             return Resize(img, resize.SourceRect, resize.TargetRect);
         }
@@ -49,10 +95,28 @@ namespace LazZiya.ImageResize
         /// </summary>
         /// <param name="img"></param>
         /// <param name="newHeight"></param>
+        /// <param name="ops">Graphic options <see cref="GraphicOptions"/></param>
         /// <returns></returns>
-        public static Image ScaleByHeight(Image img, int newHeight)
+        public static Image ScaleByHeight(this Image img, int newHeight, GraphicOptions ops)
         {
             var resize = new Scale(img.Size, new Size(0, newHeight));
+
+            return Resize(img, resize.SourceRect, resize.TargetRect, ops);
+        }
+
+        /// <summary>
+        /// Scale target image till shortest border are equal to target value, 
+        /// then crop the additonal pixels from the longest border.
+        /// Final image aspect ratio is equal to the given new width/height
+        /// </summary>
+        /// <param name="img"></param>
+        /// <param name="newWidth"></param>
+        /// <param name="newHeight"></param>
+        /// <param name="spot"></param>
+        /// <returns></returns>
+        public static Image ScaleAndCrop(this Image img, int newWidth, int newHeight, TargetSpot spot = TargetSpot.Center)
+        {
+            var resize = new ScaleAndCrop(img.Size, new Size(newWidth, newHeight), spot);
 
             return Resize(img, resize.SourceRect, resize.TargetRect);
         }
@@ -66,12 +130,13 @@ namespace LazZiya.ImageResize
         /// <param name="newWidth"></param>
         /// <param name="newHeight"></param>
         /// <param name="spot"></param>
+        /// <param name="ops">Graphic options <see cref="GraphicOptions"/></param>
         /// <returns></returns>
-        public static Image ScaleAndCrop(Image img, int newWidth, int newHeight, TargetSpot spot = TargetSpot.Center)
+        public static Image ScaleAndCrop(this Image img, int newWidth, int newHeight, GraphicOptions ops, TargetSpot spot = TargetSpot.Center)
         {
             var resize = new ScaleAndCrop(img.Size, new Size(newWidth, newHeight), spot);
 
-            return Resize(img, resize.SourceRect, resize.TargetRect);
+            return Resize(img, resize.SourceRect, resize.TargetRect, ops);
         }
 
         /// <summary>
@@ -83,10 +148,26 @@ namespace LazZiya.ImageResize
         /// <param name="newHeight"></param>
         /// <param name="spot">target spot to crop and save</param>
         /// <returns></returns>
-        public static Image Crop(Image img, int newWidth, int newHeight, TargetSpot spot = TargetSpot.Center)
+        public static Image Crop(this Image img, int newWidth, int newHeight, TargetSpot spot = TargetSpot.Center)
         {
             var resize = new Crop(img.Size, new Size(newWidth, newHeight), spot);
             return Resize(img, resize.SourceRect, resize.TargetRect);
+        }
+
+        /// <summary>
+        /// Directly crop original image without scaling it.
+        /// Final image aspect ratio is equal to given new width/height
+        /// </summary>
+        /// <param name="img"></param>
+        /// <param name="newWidth"></param>
+        /// <param name="newHeight"></param>
+        /// <param name="spot">target spot to crop and save</param>
+        /// <param name="ops">Graphic options <see cref="GraphicOptions"/></param>
+        /// <returns></returns>
+        public static Image Crop(this Image img, int newWidth, int newHeight, GraphicOptions ops, TargetSpot spot = TargetSpot.Center)
+        {
+            var resize = new Crop(img.Size, new Size(newWidth, newHeight), spot);
+            return Resize(img, resize.SourceRect, resize.TargetRect, ops);
         }
 
         /// <summary>
@@ -99,6 +180,20 @@ namespace LazZiya.ImageResize
         /// <returns></returns>
         public static Image Resize(this Image img, Rectangle source, Rectangle target)
         {
+            return img.Resize(source, target, new GraphicOptions());
+        }
+
+        /// <summary>
+        /// Specify custom resize options
+        /// </summary>
+        /// <param name="img">the image to resize</param>
+        /// <param name="source">The coordinates to read as source from the image, 
+        /// can be the whole image or part of it</param>
+        /// <param name="target">The coordinates of the target image size</param>
+        /// <param name="ops">Graphic options <see cref="GraphicOptions"/></param>
+        /// <returns></returns>
+        public static Image Resize(this Image img, Rectangle source, Rectangle target, GraphicOptions ops)
+        {
             Bitmap outputImage = new Bitmap(target.Width, target.Height, img.PixelFormat);
 
             outputImage.SetResolution(img.HorizontalResolution, img.VerticalResolution);
@@ -107,16 +202,18 @@ namespace LazZiya.ImageResize
             {
                 using (var graphics = Graphics.FromImage(outputImage))
                 {
-                    graphics.CompositingQuality = CompositingQuality.HighQuality;
-                    graphics.SmoothingMode = SmoothingMode.HighQuality;
-                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                    graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                    graphics.SmoothingMode = ops.SmoothingMode;
+                    graphics.InterpolationMode = ops.InterpolationMode;
+                    graphics.PixelOffsetMode = ops.PixelOffsetMode;
+                    graphics.CompositingQuality = ops.CompositingQuality;
+                    graphics.CompositingMode = ops.CompositingMode;
+                    graphics.PageUnit = ops.PageUnit;
 
                     graphics.DrawImage(
                         img,
                         target,
                         source,
-                        GraphicsUnit.Pixel);
+                        ops.PageUnit);
 
                 }
             }
--- LazZiya.ImageResize/SaveImage.cs
+++ LazZiya.ImageResize/SaveImage.cs
@@ -14,24 +14,24 @@ namespace LazZiya.ImageResize
         /// </summary>
         /// <param name="img">Image to save</param>
         /// <param name="path">Full path including file name and extension to save the image to</param>
-        public static void SaveAs(this Image img, string path)
+        /// <param name="quality">Image save quality (0 - 100)</param>
+        public static void SaveAs(this Image img, string path, int quality = 95)
         {
-            ImageCodecInfo myImageCodecInfo;
-            Encoder myEncoder;
-
-            var dotIndex = path.LastIndexOf('.');
-            var ext = path.Substring(dotIndex, path.Length - dotIndex - 1);
-
-            myImageCodecInfo = EncoderInfo.GetEncoderInfo(ext);
-            myEncoder = Encoder.Quality;
-
             using (var myEncoderParameters = new EncoderParameters(1))
             {
-                using (var myEncoderParameter = new EncoderParameter(myEncoder, 100L))
+                var myEncoder = Encoder.Quality;
+                using (var myEncoderParameter = new EncoderParameter(myEncoder, (long)quality))
                 {
                     myEncoderParameters.Param[0] = myEncoderParameter;
-                    img.Save(path, myImageCodecInfo, myEncoderParameters);
-                    img.Dispose();
+
+                    using (img)
+                    {
+                        var dotIndex = path.LastIndexOf('.');
+                        var ext = path.Substring(dotIndex, path.Length - dotIndex - 1);
+                        var myImageCodecInfo = EncoderInfo.GetEncoderInfo(ext);
+
+                        img.Save(path, myImageCodecInfo, myEncoderParameters);
+                    }
                 }
             }
         }
